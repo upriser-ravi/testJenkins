ed9acbc2b49cfdb6b52ebcf94c1e651567a61e8f
upriser-ravi <ravi.singh@techwisedigital.com>
2025-04-29 12:40:48 +0530

    29apr25

--- src/main/java/com/main/JenkinsTestApplication.java
+++ src/main/java/com/main/JenkinsTestApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsTestApplication {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Hello i am first build");
-		
+	public static void main(String[] args) {		
 		SpringApplication.run(JenkinsTestApplication.class, args);
 	}
 
